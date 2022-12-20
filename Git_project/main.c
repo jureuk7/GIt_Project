23963329c04cc26e5e779e4211be77578da44dc7
Hyeonmeoung Ju <wugusaud77@icloud.com>
2022-12-20 11:29:03 +0900

    Initial Commit

---
+++ Git_project/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  Git_project
+//
+//  Created by 주현명 on 2022/12/20.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
